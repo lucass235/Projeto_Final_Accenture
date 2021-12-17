795269ca62f559a9b220ef37d1477dd6fa768be8
Lucas dos Santos Amorim Rêgo <64389529+lucass235@users.noreply.github.com>
2021-12-17 10:19:23 -0300

    Add files via upload

---
+++ back/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+	private final UserRepository userRepository;
+
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@GetMapping("/users")
+	public List<User> getUsers() {
+		return (List<User>) userRepository.findAll();
+	}
+
+	@PostMapping("/users")
+	void addUser(@RequestBody User user) {
+		userRepository.save(user);
+	}
+}
